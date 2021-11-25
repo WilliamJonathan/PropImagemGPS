84473bb04644c46deccb7771a4a4b06c34a1c677
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-11-25 17:32:43 -0300

    .tratamento de exceção

--- PropImagemGPS/Program.cs
+++ PropImagemGPS/Program.cs
@@ -9,6 +9,8 @@ namespace PropImagemGPS
 
         static void Main(string[] args)
         {
+            args = new string[5];
+            args[0] = @"C:\Users\User\Documents\Downloads\img.jpg";
             DateTime datetime;
             double[] lt, lg;
             double degrees, minutes, seconds, lat_dd, long_dd;
@@ -28,64 +30,91 @@ namespace PropImagemGPS
                 string data = dll.RetornaData(path).ToString();
                 string lat = dll.RetornaLat(path).ToString();
                 string lon = dll.RetornaLong(path).ToString();*/
-                ExifReader reader = new ExifReader(path);
-                if (reader.GetTagValue<DateTime>(ExifTags.DateTimeDigitized, out datetime))
-                {
-                    data = datetime.ToString();
-                }
-                if (reader.GetTagValue<double[]>(ExifTags.GPSLatitude, out lt))//latitude
-                {
-                    reader.GetTagValue<string>(ExifTags.GPSLatitudeRef, out lat_ref);
-                    degrees = lt[0];
-                    minutes = lt[1];
-                    seconds = lt[2];
-                    lat_ref = lat_ref.ToString();
-                    if (lat_ref == "S")
-                    {
-                        lat_dd = (degrees + (minutes / 60) + (seconds / 3600)) * -1;
-                        lat = lat_dd.ToString();
-                    }
-                    else
-                    {
-                        lat_dd = degrees + (minutes / 60) + (seconds / 3600);
-                        lat = lat_dd.ToString();
-                    }
-                }
-                if (reader.GetTagValue<double[]>(ExifTags.GPSLongitude, out lg))//longitude
-                {
-                    reader.GetTagValue<string>(ExifTags.GPSLongitudeRef, out lon_ref);
-                    degrees = lg[0];
-                    minutes = lg[1];
-                    seconds = lg[2];
-                    lon_ref = lon_ref.ToString();
-                    if (lon_ref == "W")
-                    {
-                        long_dd = (degrees + (minutes / 60) + (seconds / 3600)) * -1;
-                        lon = long_dd.ToString();
-                    }
-                    else
-                    {
-                        long_dd = degrees + (minutes / 60) + (seconds / 3600);
-                        lon = long_dd.ToString();
-                    }
-                    
-                }
+                
                 try
                 {
-                    string[] lines =
+                    ExifReader reader = new ExifReader(path);
+
+                    //inicio
+                    if (reader.GetTagValue<DateTime>(ExifTags.DateTimeDigitized, out datetime))
+                    {
+                        data = datetime.ToString();
+                    }
+                    if (reader.GetTagValue<double[]>(ExifTags.GPSLatitude, out lt))//latitude
+                    {
+                        reader.GetTagValue<string>(ExifTags.GPSLatitudeRef, out lat_ref);
+                        degrees = lt[0];
+                        minutes = lt[1];
+                        seconds = lt[2];
+                        lat_ref = lat_ref.ToString();
+                        if (lat_ref == "S")
                         {
-                            "PATH="+path, "LATITUDE="+lat, "LONGITUDE="+lon, "TIRADA EM="+data, "LAT_REF=" +lat_ref, "LON_REF="+lon_ref
-                        };
+                            lat_dd = (degrees + (minutes / 60) + (seconds / 3600)) * -1;
+                            lat = lat_dd.ToString();
+                        }
+                        else
+                        {
+                            lat_dd = degrees + (minutes / 60) + (seconds / 3600);
+                            lat = lat_dd.ToString();
+                        }
+                    }
+                    if (reader.GetTagValue<double[]>(ExifTags.GPSLongitude, out lg))//longitude
+                    {
+                        reader.GetTagValue<string>(ExifTags.GPSLongitudeRef, out lon_ref);
+                        degrees = lg[0];
+                        minutes = lg[1];
+                        seconds = lg[2];
+                        lon_ref = lon_ref.ToString();
+                        if (lon_ref == "W")
+                        {
+                            long_dd = (degrees + (minutes / 60) + (seconds / 3600)) * -1;
+                            lon = long_dd.ToString();
+                        }
+                        else
+                        {
+                            long_dd = degrees + (minutes / 60) + (seconds / 3600);
+                            lon = long_dd.ToString();
+                        }
 
-                    File.WriteAllLines(pathTxt, lines);
-                    Environment.Exit(0);
+                    }
+                    try
+                    {
+                        string[] lines =
+                            {
+                        "PATH="+path, "LATITUDE="+lat, "LONGITUDE="+lon, "TIRADA EM="+data, "LAT_REF=" +lat_ref, "LON_REF="+lon_ref
+                    };
+
+                        File.WriteAllLines(pathTxt, lines);
+                        Environment.Exit(0);
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("Erro inesperado:\n" + e.Message);
+                        Environment.Exit(0);
+                    }
+                    //fim
                 }
-                catch (IOException e)
+                catch (Exception)
                 {
-                    Console.WriteLine("Erro inesperado:\n" + e.Message);
-                    Environment.Exit(0);
-                }
+                    try
+                    {
+                        string[] lines =
+                            {
+                        "Não foi possivel ler a latitude e longitude da imagem\n" +
+                        "Formato da imagem invalido!"
+                    };
 
+                        File.WriteAllLines(pathTxt, lines);
+                        Environment.Exit(0);
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("Erro inesperado:\n" + e.Message);
+                        Environment.Exit(0);
+                    }
+                    //throw;
+                }
+                
             }
 
         }
